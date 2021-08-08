9b498cf508b611b22ec89b5865b0def85e902a48
Cancid <efhale@gmail.com>
2021-08-08 11:46:49 -0400

    first commit

---
+++ db_commands.js
@@ -0,0 +1,20 @@
+let placeholders = languages.map((language) => '(?)').join(',');
+
+let sql = `SELECT DISTINCT name FROM langs
+          ORDER BY name`;
+
+db.all(sql, [], (err, rows) => {
+  if (err) {
+    throw err;
+  }
+  rows.forEach((row) => {
+    console.log(row.name);
+  });
+});
+
+db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  };
+  console.log('Close the database connection.')
+});
